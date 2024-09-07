28c522b9213c95b05c32ef582e2f0b3240c0c343
Akash Das <76689571+akash70629@users.noreply.github.com>
2024-09-07 23:58:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ class App extends Component {
       lockAspectRatio: true,
       image: null,
       resizedImage: "",
+      unit: "pixels",
     };
     this.fileChangedHandler = this.fileChangedHandler.bind(this);
     this.handleWidthChange = this.handleWidthChange.bind(this);
